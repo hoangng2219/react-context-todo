52911edb9fec8e11a30774094580c1c5467cf2e5
hoanghuphup <73218868+hoanghuphup@users.noreply.github.com>
2025-04-09 13:26:36 +0700

    chore: delete comment code

--- src/components/TaskAll.tsx
+++ src/components/TaskAll.tsx
@@ -13,31 +13,11 @@ interface TaskListProps {
   dispatch: React.Dispatch<TaskAction>;
 }
 function TaskAll({ tasks, dispatch }: TaskListProps) {
-  // const [tasks, dispatch] = useReducer(taskReducer, []);
-
-  // useEffect(() => {
-  //   if (tasks.length === 0) {
-  //     localStorage.removeItem(TASKS_KEY);
-  //     return;
-  //   }
-  //   localStorage.setItem(TASKS_KEY, JSON.stringify(tasks));
-  // }, [tasks]);
-
-  // const filteredTasks = tasks.filter((task) =>
-  //   task.text.toLowerCase().includes(searchTerm.toLowerCase())
-  // );
-  // useEffect(() => {
-  //   const storedTasks: Task[] = JSON.parse(
-  //     localStorage.getItem(TASKS_KEY) ?? "[]"
-  //   );
-  //   dispatch({ type: "LOAD_TASKS", payload: storedTasks });
-  //   // console.log("1:", storedTasks);
-  // }, []);
+  
   const deleteAll = () => dispatch({ type: "DELETE_ALL" });
   return (
     <>
       <div>
-        this is task list
         <TaskList tasks={tasks} dispatch={dispatch} />
       </div>
       <div className="flex justify-end">
